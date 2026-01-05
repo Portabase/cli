7691ef5d3dad54a2967cbae2534c9050c774273f
Théo LAGACHE <theo.lagache@soluce-technologies.com>
2026-01-05 16:40:56 +0100

    remove: old version

--- __init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = "v0.0.0"
-
-#25 = Year
-#12 = Month
-#01 = Release number in the month
-#Version number = 1,2,3...
-#Letter = a (alpha), b (beta), rc (release candidate) or nothing for stable releases
-#Beta letter = public test version before the stable version
-#Example: 25.12.1b1 = December 2025, first public beta version before the stable version
