d793b72d89b722c234813427717ddd59f56754b3
Théo LAGACHE <theo.lagache@soluce-technologies.com>
2026-01-05 11:29:47 +0100

    fix: version name

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
-__version__ = "25.12.1b3"
+__version__ = "26.01.1b2"
 
-#25 = Année
-#12 = Mois
-#Numéro de version = 1,2,3...
-#Lettre = a (alpha), b (beta), rc (release candidate) or nothing for stable releases
-#Lettre beta = version de test publique avant la version stable
-#Exemple: 25.12.1b1 = Décembre 2025, première version beta publique avant la version stable
+#25 = Year
+#12 = Month
+#01 = Release number in the month
+#Version number = 1,2,3...
+#Letter = a (alpha), b (beta), rc (release candidate) or nothing for stable releases
+#Beta letter = public test version before the stable version
+#Example: 25.12.1b1 = December 2025, first public beta version before the stable version
